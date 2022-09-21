988bff8d3c8e0d61d0108033ead4a9a7e3055bbf
Adil hussain <101266575+Adilhussain34@users.noreply.github.com>
2022-09-21 15:54:36 +0530

    Add files via upload

---
+++ scripte.js
@@ -0,0 +1,24 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from("#img5",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
+gsap.from("#main h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
